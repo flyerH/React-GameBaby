682994a9c2124acabcf17528d3f315edbe55c008
flyerH <hzw758@qq.com>
2018-10-15 22:35:05 +0800

    升级到webpack4 重启项目开发

--- config/webpack.prod.conf.js
+++ config/webpack.prod.conf.js
@@ -2,17 +2,14 @@
  * Created by He on 2017/7/10.
  * E-mail:h@strawtc.cn
  */
-const webpack = require('webpack');
 const baseWebpackConfig = require('./webpack.base.conf');
 const merge = require('webpack-merge');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const extractSass = new ExtractTextPlugin({
-  filename: 'css/style.css',
-});
 module.exports = merge(baseWebpackConfig, {
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, '../dist/static'),
     filename: 'js/[name].[chunkhash:8].js',
@@ -21,37 +18,21 @@ module.exports = merge(baseWebpackConfig, {
   },
   devtool: false,
   module: {
-    rules: [
-      {
-        test: /\.(scss|css)$/,
-        exclude: /node_modules/,
-        use: extractSass.extract({
-          fallback: 'style-loader',
-          use: [{
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[path][name]_[local]-[hash:base64:5]',
-            },
-          }, {
-            loader: 'postcss-loader',
-          }, {
-            loader: 'sass-loader',
-          }],
-        }),
-      },
-    ],
+    rules: [{
+      test: /\.(scss|css)$/,
+      exclude: /node_modules/,
+      use: [MiniCssExtractPlugin.loader, {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: '[path][name]_[local]-[hash:base64:5]',
+        },
+      }, {
+        loader: 'postcss-loader',
+      }],
+    }],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false,
-      },
-      compress: {
-        warnings: false,
-      },
-    }),
-    extractSass,
     new HtmlWebpackPlugin({
       inject: true,
       template: './public/index.html',
@@ -69,5 +50,8 @@ module.exports = merge(baseWebpackConfig, {
         minifyURLs: true,
       },
     }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css'
+    })
   ],
 });
