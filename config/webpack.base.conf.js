cbb0ecc84496f92ebef3c0ef8dfe0c6c99046247
flyerH <hzw758@qq.com>
2021-02-08 00:16:00 +0800

    stash

--- config/webpack.base.conf.js
+++ config/webpack.base.conf.js
@@ -1,28 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: './src/index.js',
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
   resolve: {
-    extensions: ['.js', '.jsx'],
-    mainFiles: ['index'],
-    alias: { '@': path.resolve(__dirname, '../src') },
+    extensions: [".js", ".jsx"],
+    mainFiles: ["index"],
+    alias: { "@": path.resolve(__dirname, "../src") },
   },
   module: {
     rules: [
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   enforce: 'pre',
+      //   loader: 'eslint-loader',
+      //   exclude: /node_modules/,
+      //   options: {
+      //     emitWarning: true,
+      //   },
+      // },
       {
         test: /\.(js|jsx)$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
         exclude: /node_modules/,
-        options: {
-          emitWarning: true,
+        use: {
+          loader: "babel-loader?cacheDirectory",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-      },
-      {
-        test: /\.(js|jsx)$/,
-        use: ['babel-loader?cacheDirectory'],
-        exclude: /node_modules/,
       },
     ],
   },
--- config/webpack.dev.conf.js
+++ config/webpack.dev.conf.js
@@ -1,60 +1,64 @@
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const { merge } = require("webpack-merge");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
-const baseWebpackConfig = require('./webpack.base.conf');
+const baseWebpackConfig = require("./webpack.base.conf");
 
 module.exports = merge(baseWebpackConfig, {
-  mode: 'development',
+  mode: "development",
   output: {
-    filename: 'js/[name].js',
-    publicPath: '/',
+    filename: "js/[name].js",
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.(scss|css)$/,
-        loaders: [
-          'style-loader',
+        use: [
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              sourceMap: true,
-              modules: true,
-              localIdentName: '[local]',
+              modules: {
+                localIdentName: "[local]",
+              },
             },
           },
           {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            },
+            loader: "sass-loader",
           },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     sourceMap: true,
+          //   },
+          // },
         ],
       },
     ],
   },
   devServer: {
-    contentBase: './public',
+    contentBase: "./public",
     historyApiFallback: true,
     hot: true,
     port: 8088,
     inline: true,
+    // open: true,
     quiet: true,
+    host: "0.0.0.0",
   },
   plugins: [
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: ['Your application is running here: http://localhost:8088'],
-      },
-      clearConsole: true,
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
+    // new FriendlyErrorsWebpackPlugin({
+    //   compilationSuccessInfo: {
+    //     notes: ['Your application is running here: http://localhost:8088'],
+    //   },
+    //   clearConsole: true,
+    // }),
+    // new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       inject: true,
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 });
